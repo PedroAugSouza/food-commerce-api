bc7f5277de1fd5b0c9690ca567714778c7785374
PedroAugSouza <psouza.880@icloud.com>
2024-08-25 19:41:59 -0300

    refractor: add 'combo' to enun category for product

---
+++ prisma/migrations/20240825224123_alter_enum_category/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `product` MODIFY `category` ENUM('DRINK', 'FOOD', 'COMBO') NOT NULL;
--- src/domain/value-objects/category-product.value-object.ts
+++ src/domain/value-objects/category-product.value-object.ts
@@ -1,4 +1,5 @@
 export enum CategoryProductValueObject {
   FOOD = 'FOOD',
   DRINK = 'DRINK',
+  COMBO = 'COMBO',
 }
